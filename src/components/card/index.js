079be589e826ade1c4a2cc310bce33f4a61d6797
KarolSm <karol.smolen2442@gmail.com>
2020-09-12 19:05:44 +0200

    Build

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -4,11 +4,11 @@ import style from './style.css';
 let list = [];
 
 console.log(style)
-const Card = (props) => <div class={style.card} > 
+const Card = () => <div class={style.card} > 
 
-<div class="col s12 m7" data-id={props.id}>
-  <div class="card horizontal">
-      <div class="card-image">
+{/* <div class="col s12 m7" data-id={props.id}>
+  <div class={style.card.horizontal}>
+      <div class={style["card-image"]}>
           <img src={props.image} />
       </div>
       <div class="card-stacked">
@@ -16,7 +16,7 @@ const Card = (props) => <div class={style.card} >
               <h5>{props.name}</h5>
               <p>{props.description}</p>
           </div>
-          <div class="buttons">
+          <div class={style.buttons}>
               <button class="button-delete btn-flat btn-large" type="delButton"><i style="line-height: 1.6;"
                       class="material-icons" data-id={props.id}>delete</i></button>
               <button class="button-edit btn-flat btn-large" type="editButton"><i style="line-height: 1.6;"
@@ -25,7 +25,7 @@ const Card = (props) => <div class={style.card} >
       </div>
   </div>
 
-</div>
+</div> */}
 </div>;
 
 const item = {
@@ -45,5 +45,5 @@ const item = {
 //     console.log("jak wyglada karta", card);
 //     list.push(card);
 //     res.send(card);
-//   });
+//   }); 
 export default Card;
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,13 +1,68 @@
-import { h } from 'preact';
-import Card from "../../components/card"
+import { h, Component } from 'preact';
+//import Card from "../../components/card"
 import style from './style.css';
+	
 
-const Home = () => (
-	<div class={style.home}>
+
+class Home extends Component {
+	state={list:[]}
+		
+
+
+	constructor() {
+	  super();
+	  this.state = { time: Date.now() };
+	}
+  
+	// Lifecycle: Called whenever our component is created
+	componentDidMount() {
+	  // update time every second
+	  fetch("http://localhost:3000/yerba")
+	  .then(response => response.json())
+	  .then(list => console.log(list));
+
+	  this.timer = setInterval(() => {
+		this.setState({ time: Date.now() });
+	  }, 1000);
+	}
+  
+	// Lifecycle: Called just before our component will be destroyed
+	componentWillUnmount() {
+	  // stop when not renderable
+	  clearInterval(this.timer);
+	}
+	
+	render() {
+		console.log("lista", list);
+		
+		return(	
+		<div class={style.home}>
 		<h1>Home</h1>
 		<p>This is the Home component.</p>
-		<Card item="ok" name="name" id="test" description="test" image="src/assets/icons/apple-touch-icon.png" />
+		<Card item={ this.State.list[0] } /> {/*tu bedzie if  */}
+		
+		<div class={style["col s12 m7"]} data-id={list.id}>
+		<div class={style["card horizontal"]}>
+			<div class={style["card-image"]}>
+				<img src={list.image} />
+			</div>
+			<div class="card-stacked">
+				<div class="card-content">
+					<h5>{list.name}</h5>
+					<p>{list.description}</p>
+				</div>
+				<div class={style.buttons}>
+					<button class="button-delete btn-flat btn-large" type="delButton"><i style="line-height: 1.6;"
+							class="material-icons" data-id={list.id}>delete</i></button>
+					<button class="button-edit btn-flat btn-large" type="editButton"><i style="line-height: 1.6;"
+							class="material-icons" data-id={list.id}>edit</i></button>
+				</div>
+			</div>
 		</div>
-);
+		</div>
+		</div>
+		);
+		}
+  }
 
 export default Home;
